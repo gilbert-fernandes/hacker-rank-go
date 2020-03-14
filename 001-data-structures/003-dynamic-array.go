9d7265d8e1682ce6c0e8afe3d4a513795fc8ad88
Gilbert Fernandes <gilbert.fernandes@orange.fr>
2020-03-14 20:36:02 +0100

    skel

--- 001-data-structures/003-dynamic-array.go
+++ 001-data-structures/003-dynamic-array.go
@@ -1,5 +1,19 @@
 package main
 
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+func dynamicArray(n int32, queries [][]int32) []int32 {
+	// Write your code here
+
+}
+
 func main() {
 
 }
