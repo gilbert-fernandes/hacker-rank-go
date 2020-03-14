d57e80a2343bc3e10d103e040dc8a4a9742ce2ae
Gilbert Fernandes <gilbert.fernandes@orange.fr>
2020-03-14 21:52:58 +0100

    go fuck yourself hacker rank. i've had enough of problems worded so poorly you can't understand what is asked. reading the comments only shows everyone is having the same problem. i'm switching to another problem website rather than losing any more minute of my precious time to your fucking crap.

--- 001-data-structures/003-dynamic-array.go
+++ 001-data-structures/003-dynamic-array.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
  * Complete the 'dynamicArray' function below.
  *
@@ -10,10 +12,42 @@ package main
  */
 
 func dynamicArray(n int32, queries [][]int32) []int32 {
-	// Write your code here
+	// so first we are asked to dynamically create an array when supplied size n
+	sequences := make([][]int32, n)
+	// parse queries. seems the first number is what is asked
+	// followed by x and y to calculate
+	for _, line := range queries {
+		query := line[0]
+		x := line[1]
+		y := line[2]
+		if query == 1 {
+			// add y to sequences ? no idea
+		} else if query == 2 {
 
+		} else {
+			fmt.Println("error : query value not in [1,2] range")
+		}
+	}
+
+	var toto []int32
+	return toto
 }
 
+/*
+ * the problem is worded so poorly that after reading 5 times the problem
+ * i have no idea what is asked for. in a professional environment, anyone daring
+ * to write specs that way would not only receive a "no" but would probably be asked
+ * to go learn how to word things properly or find another job
+ * ikbalkazar just go fuck yourself
+ *
+ */
 func main() {
-
+	sample0 := [][]int32{
+		{1, 0, 5},
+		{1, 1, 7},
+		{1, 0, 3},
+		{2, 1, 0},
+		{2, 1, 1},
+	}
+	fmt.Println(dynamicArray(5, sample0))
 }
